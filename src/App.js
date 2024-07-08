1b6522d834561fea2caba57f975ad21489e32f28
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-08 10:18:05 +0900

    Create the Projects component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
+import Projects from "./Components/Projects";
 import SocialLinks from "./Components/SocialLinks";
 
 
@@ -10,6 +11,7 @@ function App() {
      <NavBar />
      <Home />
      <About />
+     <Projects />
      <SocialLinks />
     </div>
 
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import zkNMRS from "../assets/portfolio/zk-NMRS-website.png";
+import GameHub from "../assets/portfolio/nyatoGameHub.jpg";
+import portfolioWebsite from "../assets/portfolio/portfolioWebsite.jpg";
+
+const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      src: zkNMRS,
+    },
+    {
+      id: 2,
+      src: GameHub,
+    },
+    {
+      id: 3,
+      src: portfolioWebsite,
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div
+        className="max-w-screen-lg p-4 mx-auto flex flex-col
+      justify-center w-full h-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my Projects right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;

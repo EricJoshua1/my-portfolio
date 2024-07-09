ba9bb28a26e1d60dfacbfcbf3e2fe99271762d56
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-09 13:36:39 +0900

    delete unnecesarry conflicting node package

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -43,7 +43,9 @@ const NavBar = () => {
             className="px-4 cursor-pointer capitalize font-medium
                  text-gray-500 hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
       </ul>

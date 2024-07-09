ba5545833745e30e0d586cb32fc961dfd36e599d
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-09 13:22:38 +0900

    Implement Contact Component and Install React scroll

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,15 +2,30 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <p>Contact</p>
-          <p>Feel free to get in touch with me by sending me a message below</p>
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b 
+    from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p
+            className="text-4xl font-bold inline border-b-4
+           border-gray-500"
+          >
+            Contact
+          </p>
+          <p className="py-6">
+            Feel free to get in touch with me by sending me a message below
+          </p>
         </div>
 
-        <div>
-          <form action="">
+        <div className="flex justify-center items-center ">
+          <form
+            action="https://getform.io/f/aqonnpka"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
             <input
               type="text"
               name="name"
@@ -19,14 +34,15 @@ const Contact = () => {
             />
             <input
               type="text"
-              name="name"
+              name="email"
               placeholder="Enter your email"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none "
+              className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none "
             />
             <textarea
               name="message"
+              placeholder="Enter your message"
               rows="10"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className=" p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
             <button

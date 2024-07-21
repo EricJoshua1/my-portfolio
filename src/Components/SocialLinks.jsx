52bb3a08f320cc71e932267d01a968c8197db895
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-21 18:14:47 +0900

    Change LinkedIn link

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -13,7 +13,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/eric-joshua-b23088274/",
       style: "rounded-tr-md",
     },
     {
